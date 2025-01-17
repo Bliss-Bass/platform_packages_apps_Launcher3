160f63b6f50a46114ed7948a1a6969389162195d
Jon West <electrikjesus@gmail.com>
2025-01-17 11:45:49 -0500

    Disable pref_sleep_gesture by default

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -230,7 +230,7 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
 
     @Override
     public boolean onDoubleTap(MotionEvent event) {
-        if (LauncherPrefs.getPrefs(mContext).getBoolean(SLEEP_GESTURE, true)) {
+        if (LauncherPrefs.getPrefs(mContext).getBoolean(SLEEP_GESTURE, false)) {
             VibratorWrapper.INSTANCE.get(mContext).vibrate(VibratorWrapper.EFFECT_CLICK);
             mPm.goToSleep(event.getEventTime());
         }
